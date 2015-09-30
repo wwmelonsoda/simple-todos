9938ba44166ee421083df88aca64e1dc750a74e4
Kristy ww <kristyaw@hawaii.edu>
2015-09-29 21:35:40 -1000

    finished tutorial
    finished meteor tutorial

--- simple-todos.js
+++ simple-todos.js
@@ -1,23 +1,53 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
+  // This code only runs on the client
+  Template.body.helpers({
+    tasks: [
+      { text: "This is task 1" },
+      { text: "This is task 2" },
+      { text: "This is task 3" }
+    ]
   });
 }
+Tasks = new Mongo.Collection("tasks");
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+if (Meteor.isClient) {
+    // This code only runs on the client
+    Template.body.helpers({
+        tasks: function () {
+           // Show newest tasks at the top
+            return Tasks.find({}, {sort: {createdAt: -1}});
+            return Tasks.find({});
+        }
+    });
+
+    Template.body.events({
+        "submit .new-task": function (event) {
+            // Prevent default browser form submit
+            event.preventDefault();
+
+            // Get value from form element
+            var text = event.target.text.value;
+
+            // Insert a task into the collection
+            Tasks.insert({
+                text: text,
+                createdAt: new Date() // current time
+            });
+
+            // Clear form
+            event.target.text.value = "";
+        }
+    });
+    Template.task.events({
+        "click .toggle-checked": function () {
+            // Set the checked property to the opposite of its current value
+            Tasks.update(this._id, {
+                $set: {checked: ! this.checked}
+            });
+        },
+        "click .delete": function () {
+            Tasks.remove(this._id);
+        }
+    });
 }
+
